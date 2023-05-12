b24f57e665cca1ccbcb736be37e47757a47ff5e5
Reales <rclarence123@gmail.com>
2023-05-12 01:03:40 -0500

    Testing convencional

--- testUnitario/mate_test.go
+++ testUnitario/mate_test.go
@@ -2,9 +2,28 @@ package testUnitario
 
 import "testing"
 
-func TestSumar(t *testing.T) {
-	total := sumar(5, 5)
-	if total != 11 {
-		t.Errorf("Suma incorrecta, se esperaba %d y se obtuvo %d", 11, total)
+// func TestSumar(t *testing.T) {
+// 	total := sumar(5, 5)
+// 	if total != 11 {
+// 		t.Errorf("Suma incorrecta, se esperaba %d y se obtuvo %d", 11, total)
+// 	}
+// }
+
+func TestSumar(t *testing.T){
+	tabla := []struct {
+		a int
+		b int
+		c int
+	}
+	{
+		{1, 2, 3},
+		{1, 1, 2},
+		{25, 25,50},
+	}
+
+	for _, test := range tabla {
+		if total := sumar(test.a, test.b); total != test.c {
+			t.Errorf("Suma incorrecta, se esperaba %d y se obtuvo %d", test.c, total)
+		}
 	}
 }
