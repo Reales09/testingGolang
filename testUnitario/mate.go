19b617d67ff3340586fa4276bcf0e7d5103aef73
Reales Myles <rclarence123@gmail.com>
2023-05-12 16:49:59 -0500

    Testing coverage

---
+++ testUnitario/mate.go
@@ -0,0 +1,12 @@
+package testUnitario
+
+func Suma(a, b int) int {
+	return a + b
+}
+
+func GetMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- testUnitario/mate_test.go
+++ testUnitario/mate_test.go
@@ -9,21 +9,40 @@ import "testing"
 // 	}
 // }
 
-func TestSumar(t *testing.T){
+func TestSumar(t *testing.T) {
 	tabla := []struct {
 		a int
 		b int
 		c int
-	}
-	{
+	}{
 		{1, 2, 3},
 		{1, 1, 2},
-		{25, 25,50},
+		{25, 25, 50},
 	}
 
 	for _, test := range tabla {
 		if total := sumar(test.a, test.b); total != test.c {
 			t.Errorf("Suma incorrecta, se esperaba %d y se obtuvo %d", test.c, total)
+		}
+	}
+}
+
+func TestGetMax(t *testing.T) {
+
+	tabla := []struct {
+		a int
+		b int
+
+		c int
+	}{
+		{4, 2, 4},
+		{5, 3, 5},
+		{10, 10, 10},
+	}
+	for _, item := range tabla {
+		max := GetMax(item.a, item.b)
+		if max != item.c {
+			t.Errorf("Maximo incorrecto, se esperaba %d y se obtuvo %d", item.c, max)
 		}
 	}
 }
